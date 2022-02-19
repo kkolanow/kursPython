b9ea88926ef5f6e1cb1065de79762e944903e363
kkolanow <konrad.kolanowski@gmail.com>
2022-02-19 14:46:28 +0100

    lastSunday

--- 21-DateTimeFunctions/ostatniaNiedziela.py
+++ 21-DateTimeFunctions/ostatniaNiedziela.py
@@ -1,3 +1,24 @@
 import datetime
 
+def isSameMonth(month, date):
+    return date.month == month
+
+
+this_month = datetime.date.today().month
+ 
+sunday = datetime.datetime.today()
+while(sunday.isoweekday() != 7):
+    sunday = datetime.timedelta(days=1) + sunday
+
+
+this_month = sunday.month
+
+
+
+print(this_month)
+
+while isSameMonth(this_month,sunday + datetime.timedelta(days=7)):
+    sunday = sunday + datetime.timedelta(days=7)
+
+print(sunday)
 
